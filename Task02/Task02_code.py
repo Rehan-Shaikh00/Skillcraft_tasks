c2ec0d65a32f5105b1527602d3f94422d0594bb8
Rehan-Shaikh00 <rehanshaikh898989@gmail.com>
2025-09-02 23:48:51 +0530

    Create Task02_code.py

---
+++ Task02/Task02_code.py
@@ -0,0 +1,36 @@
+📄 Report (task02_report.md)
+
+Task 02 – Data Cleaning & Exploratory Data Analysis (EDA)
+
+Dataset: Titanic (Kaggle)
+
+Tools: Python, Pandas, Seaborn, Matplotlib
+
+Process:
+
+Cleaned missing values in Age & Embarked
+
+Dropped irrelevant columns (Name, Ticket, Cabin)
+
+Encoded categorical features (Sex, Embarked)
+
+Performed exploratory analysis with plots
+
+
+Key Insights:
+
+1. Female passengers had higher survival rates than males.
+
+
+2. 1st Class passengers were more likely to survive.
+
+
+3. Age influenced survival (children had better chances).
+
+
+4. Port of embarkation also showed variation in survival rates.
+
+
+
+
+📌 Conclusion: Gender, Class, and Age were strong indicators of survival.
